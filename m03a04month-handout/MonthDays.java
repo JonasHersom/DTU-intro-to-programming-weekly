bf74d03d5b6f054e7eeb4950ac76b150310d7d91
mentallyununstable <jonasans@icloud.com>
2024-09-18 12:27:05 +0200

    work

--- m03a04month-handout/MonthDays.java
+++ m03a04month-handout/MonthDays.java
@@ -6,57 +6,63 @@ class MonthDays {
         String inputMonth = scanner.nextLine();
         int year = scanner.nextInt();
         int daysInMonth = 0;
+        boolean leap = false;
+        scanner.close();
 
         switch (inputMonth.toLowerCase()) {
             case "january":
                 daysInMonth = 31;
-
+                break;
             case "february":
-                daysInMonth = 28;
+                int remainder4 = year % 4;
+                int remainder100 = year % 100;
+                int remainder400 = year % 400;
+
+                if (remainder4 == 0) {
+                    if (remainder100 == 0) {
+                        if (remainder400 == 0) { leap = true;
+                        } else leap = false;
+                    } else leap = true;
+                } else leap = false;
+            if (leap == true) {
+                daysInMonth = 29;
+            } else {
+                daysInMonth = 28; 
+            }
+            break;
 
             case "march":
                 daysInMonth = 31;
-           
+                break;
             case "april":
                 daysInMonth = 30;
-          
+                break;
             case "may":
                 daysInMonth = 31;
+                break;
             case "june":
                 daysInMonth = 30;
-            case "juli":
+                break;
+            case "july":
                 daysInMonth = 31;
+                break;
             case "august":
                 daysInMonth = 30;
+                break;
             case "september":
                 daysInMonth = 31;
+                break;
             case "october":
                 daysInMonth = 30;
+                break;
             case "november":
                 daysInMonth = 31;
+                break;
             case "december":
                 daysInMonth = 30;
-            default:
-                daysInMonth = 31;
+                break;
         };
+        System.out.println("The month of " + inputMonth + " " + year + " has " + daysInMonth + " days");
         
-        System.out.println("The month of " + inputMonth + " has " + daysInMonth + " days");
-
-        
-        //System.out.println(month);
-
-        int remainder4 = year % 4;
-        int remainder100 = year % 100;
-        int remainder400 = year % 400;
-
-        if (remainder4 == 0) {
-            if (remainder100 == 0) {
-                if (remainder400 == 0) { System.out.println("The year " + year + " is a leap year");
-                } else System.out.println("The year " + year + " is not a leap year");
-            } else System.out.println("The year " + year + " is a leap year");
-        } else System.out.println("The year " + year + " is not a leap year");
-        
-        scanner.close();
-
     }
 }
