a1965e77cabe9e15d8b4fee3cbfc048bb98a8cb1
mentallyununstable <97104944+mentallyununstable@users.noreply.github.com>
2024-09-21 11:33:19 +0200

    Completed module 2

--- m03a07passwords-handout/Password.java
+++ m03a07passwords-handout/Password.java
@@ -1,5 +1,23 @@
 class Password {
     public static void main(String[] args) {
-        // Write your code here
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+
+        while (true) { 
+            String password = scanner.nextLine();
+            int passwordLength = password.length();
+
+            if(passwordLength < 5){
+                System.out.println("Password too short");
+                
+            } else if(passwordLength > 8){
+                System.out.println("Password too long");
+
+            } else {
+                System.out.println("Password length OK");
+                scanner.close();
+                break;
+            }
+        }
     }
 }
--- m03a08animals-handout/AnimalSounds.java
+++ m03a08animals-handout/AnimalSounds.java
@@ -1,5 +1,20 @@
 class AnimalSounds {
     public static void main(String[] args) {
-        // Write your code here
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+
+        while (scanner.hasNextLine()) { 
+            String animal = scanner.nextLine();
+            switch (animal.toLowerCase()) {
+                case "dog" -> System.out.println("Woof");
+                case "cat" -> System.out.println("Meow");
+                case "sheep" -> System.out.println("Baa");
+                case "cow" -> System.out.println("Moo");
+                case "lion" -> System.out.println("Roar");
+                case "pig" -> System.out.println("Oink");
+                case "duck" -> System.out.println("Quack");
+                default -> System.out.println("?");
+            }
     }
+}
 }
