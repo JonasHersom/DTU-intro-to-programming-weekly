0c53130c6c5f86a6021f0b6c7412b8d0f5c68b36
mentallyununstable <97104944+mentallyununstable@users.noreply.github.com>
2024-09-27 14:39:14 +0200

    Solved the rest of module 4

--- m04a06square2-handout/Utils.java
+++ m04a06square2-handout/Utils.java
@@ -1,5 +1,19 @@
 class Utils {
     static String createSquare(int size) {
-        // Write your code here
+
+        int i = size;
+        int j = size;
+        String square = "";
+
+        while (i != 0) {
+            while (j != 0) {
+                square = square + "#";
+                j = j - 1;
+            }
+            square = square + System.lineSeparator();
+            j = size;
+            i = i - 1;
     }
+    return square;
+}
 }
