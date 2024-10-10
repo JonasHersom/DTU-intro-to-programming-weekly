6269500c9f4e6e7cd64fa06e39890b68455704f9
jonashersom <97104944+jonashersom@users.noreply.github.com>
2024-10-10 09:29:09 +0200

    06-01

--- Module 6/m06a01point-handout/Point.java
+++ Module 6/m06a01point-handout/Point.java
@@ -1,1 +1,16 @@
-// Write your code here
+class Point {
+    double x;
+    double y;
+
+    static void move(Point p, double dx, double dy) {
+        p.x = p.x + dx;
+        p.y = p.y + dy;
+    }
+
+    static Point translate(Point p, double dx, double dy) {
+        Point p1 = new Point();
+        p1.x = p.x + dx;
+        p1.y = p.y + dy;
+        return p1;
+    }
+}
