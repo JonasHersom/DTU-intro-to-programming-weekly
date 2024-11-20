ac2558fcbf231415b9adeb984039131886326b91
mentallyununstable <jonasans@icloud.com>
2024-11-20 13:01:23 +0100

    completed 11/4

--- Module 11/m11a05particle-handout/Particle.java
+++ Module 11/m11a05particle-handout/Particle.java
@@ -1,1 +1,24 @@
-// Write your code here
+class Particle {
+    int particleRow;
+    int particleColumn;
+    int gridRows;
+    int gridColumns;
+    String direction;
+    
+
+    public Particle(int row, int column, int gridRows, int gridColumns, String direction) {
+        this.particleRow = row;
+        this.particleColumn = column;
+        this.gridRows = gridRows;
+        this.gridColumns = gridColumns;
+        this.direction = direction;
+    }
+
+    public int getRow() {
+        return this.particleRow;
+    }
+
+    public int getColumn() {
+        return this.particleColumn;
+    }
+}
