ee4c62035f4e525d2c7e97ecbcf122d5e39091aa
jonashersom <97104944+jonashersom@users.noreply.github.com>
2024-11-21 13:39:42 +0100

    not done with last of 11

--- Module 11/m11a05particle-handout/Particle.java
+++ Module 11/m11a05particle-handout/Particle.java
@@ -1,9 +1,9 @@
 class Particle {
-    int particleRow;
-    int particleColumn;
-    int gridRows;
-    int gridColumns;
-    String direction;
+    private int particleRow;
+    private int particleColumn;
+    private int gridRows;
+    private int gridColumns;
+    private String direction;
     
 
     public Particle(int row, int column, int gridRows, int gridColumns, String direction) {
@@ -21,4 +21,118 @@ class Particle {
     public int getColumn() {
         return this.particleColumn;
     }
+    public String getDirection() {
+        return this.direction;
+    }
+
+    public void move() {
+
+        switch (this.direction.charAt(0)) {
+            case 'N': {
+                if (this.particleRow == 0) {
+                    this.direction = this.direction.replace('N', 'S');
+                    this.particleRow++;
+                }
+                if (this.direction.charAt(0) == 'N') this.particleRow --;
+                break; 
+            }
+            case 'E': {
+                if (this.particleColumn == this.gridColumns - 1) {
+                    this.direction = this.direction.replace('E', 'W');
+                    this.particleColumn--;
+                }
+                if (this.direction.charAt(0) == 'E') this.particleColumn ++;
+                break; 
+            }
+            case 'S': {
+                if (this.particleRow == this.gridRows - 1) {
+                    this.direction = this.direction.replace('S', 'N');
+                    this.particleRow--;
+                }
+                if (this.direction.charAt(0) == 'S') this.particleRow ++;
+                break; 
+            }
+            case 'W': {
+                if (this.particleColumn == 0) {
+                    this.direction = this.direction.replace('W', 'E');
+                    this.particleColumn ++;
+                }
+                if (this.direction.charAt(0) == 'W') this.particleColumn --;
+                break; 
+            }
+            default: break; 
+        }
+
+        // second direction
+
+        if (this.direction.length() == 2) {
+            switch (this.direction.charAt(1)) {
+                case 'N': {
+                    if (this.particleRow == 0) {
+                        this.direction = this.direction.replace('N', 'S');
+                        this.particleRow++;
+                    }
+                    if (this.direction.charAt(1) == 'N') this.particleRow --;
+                    break; 
+                }
+                case 'E': {
+                    if (this.particleColumn == this.gridColumns - 1) {
+                        this.direction = this.direction.replace('E', 'W');
+                        this.particleColumn--;
+                    }
+                    if (this.direction.charAt(1) == 'E') this.particleColumn ++;
+                    break; 
+                }
+                case 'S': {
+                    if (this.particleRow == this.gridRows - 1) {
+                        this.direction = this.direction.replace('S', 'N');
+                        this.particleRow--;
+                    }
+                    if (this.direction.charAt(1) == 'S') this.particleRow ++;
+                    break; 
+                }
+                case 'W': {
+                    if (this.particleColumn == 0) {
+                        this.direction = this.direction.replace('W', 'E');
+                        this.particleColumn ++;
+                    }
+                    if (this.direction.charAt(1) == 'W') this.particleColumn --;
+                    break; 
+                }
+                default: break; 
+            }
+        }
+    }
+
+    public String display() {
+        StringBuilder grid = new StringBuilder();
+    
+        grid.append("+");
+        for (int i = 0; i < this.gridColumns; i++) {
+            grid.append("-");
+        }
+        grid.append("+\n");
+    
+        for (int i = 0; i < this.gridRows; i++) {
+            grid.append("|");
+            for (int j = 0; j < this.gridColumns; j++) {
+                if (i == this.particleRow && j == this.particleColumn) {
+                    grid.append("*"); 
+                } else {
+                    grid.append(" "); 
+                }
+            }
+            grid.append("|\n");
+        }
+    
+        grid.append("+");
+        for (int i = 0; i < this.gridColumns; i++) {
+            grid.append("-");
+        }
+        grid.append("+\n");
+    
+        grid.append("Position: (" + this.particleRow + "," + this.particleColumn + "); direction: " + this.direction + System.lineSeparator());
+        return grid.toString();
+    }
+    
 }
--- Module 11/m11a07contacts-handout/Contact.java
+++ Module 11/m11a07contacts-handout/Contact.java
@@ -1,5 +1,76 @@
 class Contact {
-    // Write your code here
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String name, String surname, String phone, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phone;
+        this.email= email;
+        isValid();
+    }
+
+    private void isValid() {
+        if (this.name == null || name.length() < 1) throw new IllegalArgumentException("Invalid name");
+        if (this.surname == null || surname.length() < 1) throw new IllegalArgumentException("Invalid surname");
+        if (this.phoneNumber == null || phoneNumber.length() < 1) throw new IllegalArgumentException("Invalid phone number");
+        if (this.email == null || email.length() < 1) throw new IllegalArgumentException("Invalid email");
+
+        if (this.phoneNumber.charAt(0) == '+' || isDigit(this.phoneNumber.charAt(0))) ;
+        else throw new IllegalArgumentException ("Invalid phone number");
+
+        int digits = 0;
+        for (int i = 1; i < this.phoneNumber.length(); i++) {
+            if (!isDigit(this.phoneNumber.charAt(i))) throw new IllegalArgumentException ("Invalid phone number");
+            else digits++;
+        }
+        if (digits < 2) throw new IllegalArgumentException ("Invalid phone number");
+
+        var emailSplit = email.split("@");
+        if (emailSplit.length != 2) throw new IllegalArgumentException("Invalid email");
+        if (emailSplit[0].isEmpty() || emailSplit[1].isEmpty()) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+        
+        for (int i = 0; i < emailSplit.length; i++) {
+            for (int j = 0; j < emailSplit[i].length(); j++) {
+                if (isDigit(emailSplit[i].charAt(j)) || isLetter(emailSplit[i].charAt(j)) || emailSplit[i].charAt(j) == '.');
+                else throw new IllegalArgumentException("Invalid email");
+            }
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getSurname() {
+        return this.surname;
+    }
+    public String getPhone() {
+        return this.phoneNumber;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        isValid();
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+        isValid();
+    }
+    public void setPhone(String phone) {
+        this.phoneNumber = phone;
+        isValid();
+    }
+    public void setEmail(String email) {
+        this.email = email;
+        isValid();
+    }
 
     /**
      * Is the given character a digit?
